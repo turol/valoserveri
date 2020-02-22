1d17884b4fbec1476e5e67bd99e5392cd76fb267
Turo Lamminen <turol@iki.fi>
2020-02-22 19:57:34 +0200

    Fix no websockets build

--- valoserveri/serveri.cpp
+++ valoserveri/serveri.cpp
@@ -12,43 +12,6 @@
 using namespace valoserveri;
 
 
-#ifdef USE_LIBWEBSOCKETS
-
-
-/* one of these created for each message */
-
-struct msg {
-	void *payload; /* is malloc'd */
-	size_t len;
-};
-
-
-/* one of these is created for each client connecting to us */
-
-struct per_session_data__minimal {
-	struct per_session_data__minimal *pss_list;
-	struct lws *wsi;
-	int last; /* the last message number we sent */
-};
-
-
-/* one of these is created for each vhost our protocol is used with */
-
-struct per_vhost_data__minimal {
-	struct lws_context *context;
-	struct lws_vhost *vhost;
-	const struct lws_protocols *protocol;
-
-	struct per_session_data__minimal *pss_list; /* linked-list of live pss*/
-
-	struct msg amsg; /* the one pending message... */
-	int current; /* the current message number we are caching */
-};
-
-
-static int callback(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len);
-
-
 class Serveri {
 	int                 UDPfd;
 
@@ -100,6 +63,43 @@ public:
 
 // TODO: hax, remove
 static Serveri *globalServeri = nullptr;
+
+
+#ifdef USE_LIBWEBSOCKETS
+
+
+/* one of these created for each message */
+
+struct msg {
+	void *payload; /* is malloc'd */
+	size_t len;
+};
+
+
+/* one of these is created for each client connecting to us */
+
+struct per_session_data__minimal {
+	struct per_session_data__minimal *pss_list;
+	struct lws *wsi;
+	int last; /* the last message number we sent */
+};
+
+
+/* one of these is created for each vhost our protocol is used with */
+
+struct per_vhost_data__minimal {
+	struct lws_context *context;
+	struct lws_vhost *vhost;
+	const struct lws_protocols *protocol;
+
+	struct per_session_data__minimal *pss_list; /* linked-list of live pss*/
+
+	struct msg amsg; /* the one pending message... */
+	int current; /* the current message number we are caching */
+};
+
+
+static int callback(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len);
 
 
 /* destroys the message when everyone has had a copy of it */
